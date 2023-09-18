2f00251cb541276d5c10d12809911f01de5200a6
Alex Alaniz <obyc@okaybearsyachtclub.io>
2023-09-17 21:58:52 -0400

    Updated

---
+++ consts/contractAddresses.ts
@@ -0,0 +1,6 @@
+export const nftDropContractAddress =
+  "0xeA379633bDabd2A8232AA7bAD1E5FD1D9940dB6f";
+export const tokenContractAddress =
+  "0xdC58817Daa6eA688e54d1F5C4dA207B4E49Be6bb";
+export const stakingContractAddress =
+  "0x918D6f44BCe75F1dE407211DA190329970BD0370";
