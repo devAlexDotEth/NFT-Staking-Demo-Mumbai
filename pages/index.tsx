93aae38d3e848e124ee20145cf817e5c298f4112
Alex Alaniz <obyc@okaybearsyachtclub.io>
2023-09-18 00:21:03 -0400

    Updates

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       {/* Top Section */}
-      <h1 className={styles.h1}>Wilf Child NFT - Staking Dapp - $WILD</h1>
+      <h1 className={styles.h1}>Wild Child NFT - Staking Dapp - $WILD</h1>
       <div className={styles.nftBoxGrid}>
         <div
           className={styles.optionSelectBox}
