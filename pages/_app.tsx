febb32eeebce74fea10f8a14501a17d00cae9368
devAlex.ᴱᵀᴴ <88956822+devAlexDotEth@users.noreply.github.com>
2023-09-20 14:27:26 -0400

    Bearified™ Labs - Demo Staking Dapp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import { ThirdwebProvider } from "@thirdweb-dev/react";
 import type { AppProps } from "next/app";
 import "../styles/globals.css";
+import {
+  metamaskWallet,
+  coinbaseWallet,
+  walletConnect,
+  safeWallet,
+  localWallet,
+  trustWallet,
+  zerionWallet,
+  bloctoWallet,
+  frameWallet,
+  rainbowWallet,
+  phantomWallet,
+} from "@thirdweb-dev/react";
 
 // This is the chain your dApp will work on.
 const activeChain = "mumbai";
@@ -10,6 +23,25 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ThirdwebProvider
       activeChain={activeChain}
       clientId={process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID}
+      supportedWallets={[
+        metamaskWallet(),
+        coinbaseWallet(),
+        walletConnect(),
+        safeWallet({
+          personalWallets: [
+            metamaskWallet(),
+            coinbaseWallet(),
+            walletConnect(),
+          ],
+        }),
+        localWallet(),
+        trustWallet(),
+        zerionWallet(),
+        bloctoWallet(),
+        frameWallet(),
+        rainbowWallet(),
+        phantomWallet(),
+      ]}
     >
       <Component {...pageProps} />
     </ThirdwebProvider>
