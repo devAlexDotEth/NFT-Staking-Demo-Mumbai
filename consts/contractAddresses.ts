23c36f5c91ca1c69e6140d906a896a0e45105583
devAlex.ᴱᵀᴴ <88956822+devAlexDotEth@users.noreply.github.com>
2023-09-20 19:12:41 -0400

    Bearified™ Labs - Demo Staking Dapp

--- consts/contractAddresses.ts
+++ consts/contractAddresses.ts
@@ -1,6 +1,6 @@
 export const nftDropContractAddress =
   "0xb0fC489Ce5a0C43B6b3DeF092769051672395b7e";
 export const tokenContractAddress =
-  "0x5e56646395d003584C8bB15b05ba015e2a40eA03";
+  "0xA4aA69Ac83287C83744Ef03A69Cf5370d1f6468a";
 export const stakingContractAddress =
-  "0x71746DDC2b3e66141988CB6516CB239Bd5223D0c";
+  "0x852dF0486Ab81cDad900DB10F6a7e8bBd87EB145";
